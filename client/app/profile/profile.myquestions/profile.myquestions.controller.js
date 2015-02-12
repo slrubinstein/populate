b06e05c05fb9c764edec6f8e6b3d682b596bfa8c
Steve Rubinstein <slrubinstein@gmail.com>
2015-02-12 17:37:57 -0500

    user can view questions they have voted on from profile page

--- client/app/profile/profile.myquestions/profile.myquestions.controller.js
+++ client/app/profile/profile.myquestions/profile.myquestions.controller.js
@@ -9,8 +9,8 @@ function ProfileMyquestionsCtrl($stateParams, highcharts, userQuestionService, A
 	
   var vm = this;
 
+  vm.endOfList;
   vm.list = $stateParams.options.list || 'myQuestionsCurrent';
-
   vm.index = $stateParams.options.value || 0;
   vm.next = next;
   vm.previous = previous;
@@ -22,7 +22,9 @@ function ProfileMyquestionsCtrl($stateParams, highcharts, userQuestionService, A
     .then(function() {
     	vm.myQuestionsCurrent = userQuestionService.myQuestionsCurrent;
       vm.myQuestionsOld = userQuestionService.myQuestionsOld;
+      vm.friendQuestionsOld = userQuestionService.friendQuestionsOld;
       vm.currentQuestion = vm[vm.list][vm.index];
+      vm.endOfList = vm.index === vm[vm.list].length - 1 ? true : false;
     })
   }
   
@@ -30,10 +32,14 @@ function ProfileMyquestionsCtrl($stateParams, highcharts, userQuestionService, A
   function next() {
     vm.index++;
     vm.currentQuestion = vm[vm.list][vm.index];
+    if (vm.index === vm[vm.list].length - 1) {
+      vm.endOfList = true;
+    }
   }
 
   function previous() {
     vm.index--;
     vm.currentQuestion = vm[vm.list][vm.index];
+    vm.endOfList = false;
   }
 }
